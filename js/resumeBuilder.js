021c6f4bddb20f225b0be81a7d389b8d0daf64f7
Dean Throop <DThroop@PacificCabinets.com>
2014-10-12 14:07:02 -0700

    Initial javascript tests

---
+++ js/resumeBuilder.js
@@ -0,0 +1,6 @@
+var firstName="Dean";
+var awesomeThoughts="I am "+firstName+" and I am AWESOME!";
+var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
+console.log(funThoughts);
+
+$("#main").append(funThoughts);
