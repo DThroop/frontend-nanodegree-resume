905730d90f44deedbcf98d78265683b7c547799d
Dean Throop <DThroop@PacificCabinets.com>
2014-10-12 14:25:13 -0700

    Header revision quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,18 @@
-var firstName="Dean";
-var awesomeThoughts="I am "+firstName+" and I am AWESOME!";
-var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
-console.log(funThoughts);
+//var firstName="Dean";
+//var awesomeThoughts="I am "+firstName+" and I am AWESOME!";
+//var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
+//console.log(funThoughts);
 
-$("#main").append(funThoughts);
+//$("#main").append(funThoughts);
+
+var name="Dean Throop";
+var role="Software Engineer";
+
+var formattedName=HTMLheaderName.replace("%data%", name);
+var formattedRole=HTMLheaderRole.replace("%data%", role);
+
+//console.log(formattedName);
+//console.log(formattedRole);
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
