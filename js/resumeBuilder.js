53eed0b01418a19ead57005b723eef640b6d8e5e
Dean Throop <DThroop@PacificCabinets.com>
2014-10-12 19:37:12 -0700

    Added more objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,14 +5,57 @@
 
 //$("#main").append(funThoughts);
 
-var name="Dean Throop";
-var role="Software Engineer";
+//var name="Dean Throop";
+//var role="Software Engineer";
 
-var formattedName=HTMLheaderName.replace("%data%", name);
-var formattedRole=HTMLheaderRole.replace("%data%", role);
+var skills=["awesomeness","programming","teaching","JS"];
+
+//$("#main").append(skills[0]);
+
+//$("#main").append(skills.length);
+
+var bio={
+  "name" : "Dean Throop",
+  "role" : "Software Engineer",
+  "contacts" : {
+    "mobile" : "208-305-4361",
+    "email" : "DThroop@TrackITServices.com",
+    "github" : "DThroop",
+    "twitter" : "@Dean.Throop",
+    "location" : "Craigmont, ID"
+  },
+  "BioPic" : "images/fry.jpg",
+  "welcome" : "Howdy ho neighbor!",
+  "skills" : skills
+};
+
+var currentJob={};
+currentJob["position"]="IT Manager";
+currentJob["employer"]="Pacific Cabinets, Inc.";
+currentJob["years"]=10;
+currentJob["city"]="Ferdinand";
+
+var education={};
+education.name="University of Idaho";
+education.years=4;
+education.city="Moscow, ID";
+
+//var formattedName=HTMLheaderName.replace("%data%", bio.name);
+//var formattedRole=HTMLheaderRole.replace("%data%", bio.role);
 
 //console.log(formattedName);
 //console.log(formattedRole);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+$("#header").prepend(HTMLskills.replace("%data%",bio.skills));
+$("#header").prepend(HTMLlocation.replace("%data%",bio.contacts.location));
+$("#header").prepend(HTMLgithub.replace("%data%",bio.contacts.github));
+$("#header").prepend(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+$("#header").prepend(HTMLemail.replace("%data%",bio.contacts.email));
+$("#header").prepend(HTMLmobile.replace("%data%",bio.contacts.mobile));
+$("#header").prepend(HTMLWelcomeMsg.replace("%data%",bio.welcome));
+$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+$("#header").prepend(HTMLbioPic.replace("%data%",bio.BioPic));
+
+$("#main").append(currentJob.position);
+$("#main").append(education["name"]);
